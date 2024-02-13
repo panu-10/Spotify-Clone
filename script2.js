0cc73feb83b14842fe5a453e9512b4845a783ccf
Pranav Pawar <146022425+panu-10@users.noreply.github.com>
2024-02-13 19:37:22 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,12 @@
 console.log("Welcome to Spotify");
 
-let songIndex=0;
-let audioElement=new Audio('songs/1.mp3');
-let masterPlay=document.getElementById('masterPlay');
-let myProgressBar=document.getElementById('myProgressBar');
-let gif=document.getElementById('gif');
-let masterSongName=document.getElementById('masterSongName');
-let songItems=Array.from(document.getElementsByClassName('songItem'));
-
+let songIndex = 0;
+let audioElement = new Audio();
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
     {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
@@ -19,104 +18,70 @@ let songs = [
     {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
     {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
     {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
-]
-songItems.forEach((element,i)=>{
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
-masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-          audioElement.play();
-          masterPlay.classList.remove('fa-play-circle');
-          masterPlay.classList.add('fa-pause-circle');
-          gif.style.opacity=1;
-    }
-    else{
+];
+
+// Function to initialize audio element with a given song index
+const initializeAudioElement = (index) => {
+    audioElement.src = songs[index].filePath;
+    masterSongName.innerText = songs[index].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+    gif.style.opacity = 1;
+};
+
+// Function to toggle play/pause state of the audio
+const togglePlayPause = () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    } else {
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity=0;
+        gif.style.opacity = 0;
     }
-})
+};
 
-audioElement.addEventListener('timeupdate',()=>{
-    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
-    myProgressBar.value=progress;
-})
+// Function to update progress bar based on audio current time
+const updateProgressBar = () => {
+    const progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    myProgressBar.value = progress;
+};
 
-myProgressBar.addEventListener('change',()=>{
-   audioElement.currentTime=myProgressBar.value * audioElement.duration/100;
-})
+// Initialize audio element with the first song
+initializeAudioElement(songIndex);
 
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.remove('fa-pause-circle');
-        element.classList.add('fa-play-circle');
-    })
-}
+// Event listener for master play/pause button
+masterPlay.addEventListener('click', togglePlayPause);
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
-})
-document.getElementById('next').addEventListener('click',()=>{
-    if(songIndex>=9){
-        songIndex=0;
-    }
-    else{
-        songIndex+=1;
-    }
-    audioElement.src=`songs/${songIndex+1}.mp3`;
-    masterSongName.innerText=songs[songIndex].songName;
-    audioElement.currentTime=0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.remove('fa-pause-circle');
-})
+// Event listener for time update of audio element
+audioElement.addEventListener('timeupdate', updateProgressBar);
 
-document.getElementById('previous').addEventListener('click',()=>{
-    if(songIndex<=0){
-        songIndex=0;
-    }
-    else{
-        songIndex-=1;
-    }
-    audioElement.src=`songs/${songIndex+1}.mp3`;
-    masterSongName.innerText=songs[songIndex].songName;
-    audioElement.currentTime=0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.remove('fa-pause-circle');
-})
+// Event listener for progress bar change
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+});
 
+// Event listener for song item play buttons
+songItems.forEach((element, i) => {
+    element.getElementsByClassName('songItemPlay')[0].addEventListener('click', () => {
+        songIndex = i;
+        initializeAudioElement(songIndex);
+    });
+});
 
+// Event listener for next button
+document.getElementById('next').addEventListener('click', () => {
+    songIndex = (songIndex + 1) % songs.length;
+    initializeAudioElement(songIndex);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Event listener for previous button
+document.getElementById('previous').addEventListener('click', () => {
+    songIndex = (songIndex - 1 + songs.length) % songs.length;
+    initializeAudioElement(songIndex);
+});
